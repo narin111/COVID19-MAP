4438ad74c7c0ebdcae3b8ce11c4882a9e5722bb5
narin <narin1101@gmail.com>
2020-11-09 23:53:38 +0900

    지도표시 위치서울

--- cmapsite/corona/views.py
+++ cmapsite/corona/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("BBByyyeeee corona")
+    return render(request, 'corona/index.html')
+
