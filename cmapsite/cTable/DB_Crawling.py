26ece52787a4278e655c6ad6ccf44c2d775365a0
dayoung <ekffkddl0529@naver.com>
2021-01-01 22:15:26 +0900

    update db_crawling

--- cmapsite/cTable/DB_Crawling.py
+++ cmapsite/cTable/DB_Crawling.py
@@ -1,13 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+#웹페이지 소스코드 가져오기
 url='https://www.jeonbuk.go.kr/index.jeonbuk?menuCd=DOM_000000110006000000'
 html=urllib.request.urlopen(url).read()
 soup=BeautifulSoup(html, 'html.parser')
 
+#현재 페이지에서 table 태그 모드 선택하기
 table=soup.find('table', {'class':'covid_table'})
 trs = table.find_all('tr')
-data = [] #[도시, 군구, 상점이름...배열]
+
+#[도시, 군구, 상점이름...배열]
+data = [] 
+
 for idx, tr in enumerate(trs):
     if (idx) > 0:
         tds=tr.find_all('td')
@@ -18,7 +23,3 @@ for idx, tr in enumerate(trs):
         date = tds[4].text.strip()
         sterilization_status = tds[5].text.strip()
         data.append([city, county, store_name, address, date, sterilization_status])
-
-print(data)
-        
-
