0c82f74399a523438e0c6e48afec09ab71ab9a77
dayoung <ekffkddl0529@naver.com>
2021-01-01 23:22:22 +0900

    add route_table in views.py

--- cmapsite/cTable/views.py
+++ cmapsite/cTable/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from DB_Crawling import route_table
 
 # Create your views here.
+
+def route_table(request):
+    routTable = route_table()
+    return render(request, 'cTable/cTable.html', rout_table)
