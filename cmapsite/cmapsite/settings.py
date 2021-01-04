ef04b4756c28e377138d239291ac49088f7b2c1f
narin <narin1101@gmail.com>
2021-01-04 16:08:52 +0900

    markertest

--- cmapsite/cmapsite/settings.py
+++ cmapsite/cmapsite/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corona',
     'corona.apps.CoronaConfig',
     'cTable.apps.CtableConfig',
 ]
@@ -138,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
