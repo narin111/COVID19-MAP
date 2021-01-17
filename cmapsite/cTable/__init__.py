ab5064c28e8fa2673575527227c4b1bb61dccc43
ygh265245 <ygh265245@gmail.com>
2021-01-18 00:19:11 +0900

    지도 - 표 연결

--- cmapsite/cTable/__init__.py
+++
@@ -1,2 +0,0 @@
-import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
--- cmapsite/cmapsite/settings.py
+++ cmapsite/cmapsite/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corona.apps.CoronaConfig',
-    'cTable.apps.CtableConfig',
+   
 ]
 
 MIDDLEWARE = [
